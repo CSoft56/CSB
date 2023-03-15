cc401e02f67e5f85a9b87c5e0de68099ba344428
vemulapalli2 <vemulapallimahitha93@gmail.com>
2023-03-15 13:56:35 -0400

    putting back the original code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 Main for execution of CSB
 """
 acc = {
-    "AccNo": "A550",
-    "Balance": 50000,
-    "Name": "Mahitha",
-    "Transactions": [{"type": "debit", "amount": 50000, "date": "2023-03-13"}]
+    "AccNo": "A100",
+    "Balance": 20000,
+    "Name": "Venkat",
+    "Transactions": [{"type": "debit", "amount": 20000, "date": "2023-03-12"}]
 }
 
 
@@ -19,31 +19,18 @@ def deposit():
 
 
 def withdraw():
-    amt = int(input("Enter the amount to withdraw: "))
-    acc["withdraw"] = amt
-    # acc["withdraw"] = acc["withdraw"] amt
-    print("Transaction completed successfully")
-    print("Your New Balance is: {}".format(acc["balance"]))
+    print("Inside withdraw()")
     show_options()
 
 
 def balance():
-    amt = int(input("Enter the balance amount: "))
-    acc["Balance"] += amt
-    # acc["Balance"] = acc["Balance"] + amt
-    print("Inside Balance amount ")
-    print("Your New Balance is: {}".format(acc["Balance"]))
+    print("Inside balance()")
     show_options()
 
 
 def mini_statement():
-    amt = int(input("Enter the mini_statement: "))
-    acc["Balance"] += amt
-    # acc["Balance"] = acc["Balance"] + amt
-    print("Inside mini_statement ")
-    print("Your New Balance is: {}".format(acc["Balance"]))
+    print("Inside mini_statement()")
     show_options()
-
 
 
 def show_options():
@@ -73,4 +60,3 @@ if __name__ == '__main__':
             ch_func_mapping[ch]()
         else:
             print("Invalid Choice, Please enter Valid choice from 1-5")
-
